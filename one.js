2128660cb77ee3b98cc80615ee09c7e9dbbbc3b5
prasanthpalanisam <140495047+prasanthpalanisam@users.noreply.github.com>
2023-07-31 16:09:26 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,6 @@
+function off(){
+        document.getElementById('img').src='https://www.collinsdictionary.com/images/full/lightbulb_111547856_1000.jpg';
+    }
+    function on(){
+        document.getElementById('img').src='https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000';
+    }
